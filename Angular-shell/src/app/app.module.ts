74647a45041499938470828e9b38d910fcf64c8b
buitheanh2802 <theanhbui345@gmail.com>
2025-03-12 16:35:11 +0700

    2222

--- Angular-shell/src/app/app.module.ts
+++ Angular-shell/src/app/app.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from "@angular/core";
 import { AppComponent } from "./app.component";
 import { AppRoutingModule } from "./app-routing.module";
 import { BrowserModule } from "@angular/platform-browser";
+import { NzButtonModule } from "ng-zorro-antd/button";
 
 @NgModule({
     declarations: [
@@ -9,7 +10,8 @@ import { BrowserModule } from "@angular/platform-browser";
     ],
     imports: [
       AppRoutingModule,
-      BrowserModule
+      BrowserModule,
+      NzButtonModule
     ],
     providers: [],
     bootstrap: [
